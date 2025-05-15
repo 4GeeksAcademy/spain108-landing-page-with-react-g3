6264a9ed3733957b329f3775b1d1c4430faf0e44
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-05-15 18:08:18 +0000

    [Jumbotron] feat: Jumbotron

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,18 @@
+
+export const Jumbotron = () => {
+    return (
+        <div className="container">
+            <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+                <div className="container-fluid py-5"> <h1 className="display-5 fw-bold">A Warm Welcome</h1>
+                    <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur hic ea sint laudantium distinctio, cum voluptatibus quibusdam iure quas non aut sed exercitationem libero nulla ducimus iste corporis esse. Culpa.</p>
+                    <button className="btn btn-primary btn-lg" type="button">Call to action</button>
+                </div>
+            </div>
+        </div>
+
+
+    )
+
+
+}
+
