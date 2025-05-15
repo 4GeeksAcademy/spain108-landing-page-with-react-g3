20df9d4b7164cab24886b001decfd74f8d9b0d7b
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-05-15 18:51:14 +0000

    [footer] feat: footer

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+return (
+    <footer className="bg-dark text-center text-white py-3 mt-5">
+  <div className="container">
+    <small>Copyright © Your Website 2019</small>
+  </div>
+</footer>
+)
+}
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Jumbotron } from "./Jumbotron";
 import Cards from "../components/Cards";
+import { Footer } from "./Footer";
 //include images into your bundle
 
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-       <Jumbotron/>
-	   <Cards title="Hombre con sombrero"/>
-	   <Cards title="Señorita invierno"/>
-	   
-		</div>
+		<>
+			<div className="d-flex flex-column min-vh-100 text-center ">
+				<Jumbotron />
+				<Cards title="Hombre con sombrero" />
+				<Cards title="Señorita invierno" />
+				<footer />
+			</div>
+			<Footer/>
+		</>
 	);
 };
 
